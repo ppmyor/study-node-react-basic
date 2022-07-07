08f0fcd13fed4509f08ff198dc46d85ea6d7e1d6
ppmyor <yorocobe@gmail.com>
2022-07-07 15:44:48 +0900

    feat: 간단한 회원가입 라우터 생성

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb+srv://anna:abcd1234@boiler-plate.vms94mj.mongodb.net/?retryWrites=true&w=majority")
@@ -10,6 +16,17 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
