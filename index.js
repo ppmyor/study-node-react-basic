dbd3a9bbcff9c5cd7ca9a828c1e1fcf8dd2b2227
ppmyor <yorocobe@gmail.com>
2022-07-09 22:48:53 +0900

    feat: 로그아웃 기능 추가

--- index.js
+++ index.js
@@ -58,8 +58,6 @@ app.post("/api/users/login", (req, res) => {
 });
 
 app.get("/api/users/auth", auth, (req, res) => {
-  // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication이 true라는 말.
-
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -69,6 +67,15 @@ app.get("/api/users/auth", auth, (req, res) => {
     lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
   });
 });
 
