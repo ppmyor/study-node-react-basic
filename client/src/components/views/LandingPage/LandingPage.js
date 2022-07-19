22aa7579cf31e1724e38bd73452dd566cbae3c3b
김안나 <yorocobe@gmail.com>
2022-07-20 01:51:13 +0900

    feat: 로그인 페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,11 @@ function LandingPage() {
   useEffect(() => {
     axios.get("api/hello").then((response) => console.log(response.data));
   });
-  return <>LandingPage</>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100vh" }}>
+      <h2>시작페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
